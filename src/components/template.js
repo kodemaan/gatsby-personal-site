99aba2127bc39d9603d8bf91d2f94623a884c915
Steven Smith <stsmith@nabancard.com>
2020-07-08 21:02:46 -0400

    Committing latest changes

--- src/components/template.js
+++ src/components/template.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import { Link } from 'gatsby';
-import templateStyles from './template.module.css';
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'gatsby'
+import templateStyles from './template.module.css'
 
 const Template = ({ children }) =>
-  <div className={templateStyles.title}>
-    <h1>My Personal Site</h1>
+  <div className={templateStyles.body}>
+    <div className={templateStyles.title}>
+      <h1>My Personal Site</h1>
+    </div>
     <div className={templateStyles.navigation}>
-      <div className={templateStyles.link}><Link to="/about">Home</Link></div>
+      <div className={templateStyles.link}><Link to="/">Home</Link></div>
       <div className={templateStyles.link}><Link to="/about">About Me</Link></div>
     </div>
-    <nav></nav>
     <img src="https://source.unsplash.com/random/400x200" alt="" />
     {children}
-    <Link to="/about/">About me</Link>
   </div>
+
+Template.propTypes = {
+  children: PropTypes.element
+}
 export default Template
